8f2a270ac352d1b94233209e66e1687e3a0e5a7a
Walther Zapata Casas <walther.zapata@ias.com.co>
2025-05-26 15:28:22 -0500

    Initial creation of the software simulator

---
+++ application/web-application/src/main/java/co/com/simulator/SoftwareSimulatorApplication.java
@@ -0,0 +1,13 @@
+package co.com.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareSimulatorApplication.class, args);
+	}
+
+}
---
+++ application/web-application/src/test/java/co/com/simulator/SoftwareSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
